205a34c2f96ad953f504ed884ac7686ec14ed2a2
anji5h <40964308+anji5h@users.noreply.github.com>
2021-09-04 19:42:00 +0545

    prod build

--- mongoose.js
+++ mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  `${process.env.MONGO_LOCAL_URL}`,
+  `${process.env.MONGO_URL}`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
